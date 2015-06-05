0ebe5da61db89770cfb09d459d68c85faef7421c
Sean Scott <sas75@duke.edu>
2015-06-05 16:37:37 -0400

    chartfx files and action update

---
+++ chart samples/Business_CLR_Objects/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace SoftwareFX.Samples._Business_CLR_Objects
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxSamples = new System.Windows.Forms.ComboBox();
+            this.chart1 = new ChartFX.WinForms.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Sample:";
+            // 
+            // comboBoxSamples
+            // 
+            this.comboBoxSamples.FormattingEnabled = true;
+            this.comboBoxSamples.Location = new System.Drawing.Point(61, 7);
+            this.comboBoxSamples.Name = "comboBoxSamples";
+            this.comboBoxSamples.Size = new System.Drawing.Size(165, 21);
+            this.comboBoxSamples.TabIndex = 4;
+            this.comboBoxSamples.SelectedIndexChanged += new System.EventHandler(this.comboBoxSamples_SelectedIndexChanged);
+            // 
+            // chart1
+            // 
+            this.chart1.AllowDrop = true;
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart1.AxisX.Title.Separation = 10;
+            this.chart1.AxisY.Title.Separation = 10;
+            this.chart1.AxisY2.Title.Separation = 10;
+            this.chart1.Location = new System.Drawing.Point(7, 38);
+            this.chart1.MainPane.Title.Separation = 5;
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(833, 410);
+            this.chart1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 460);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxSamples);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxSamples;
+        private ChartFX.WinForms.Chart chart1;
+    }
+}
---
+++ chart samples/Business_CLR_Objects/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Data.SqlServerCe;
+using System.Collections;
+using System.Diagnostics;
+using ChartFX.WinForms;
+using ChartFX.WinForms.Annotation;
+using ChartFX.WinForms.Adornments;
+using ChartFX.WinForms.Galleries;
+using ChartFX.WinForms.Internal.UI;
+using SoftwareFX.WinForms;
+using SoftwareFX.WinForms.Data;
+using SoftwareFX.WinForms.Data.Expressions;
+
+namespace SoftwareFX.Samples._Business_CLR_Objects
+{
+    public partial class Form1 : Form
+    {
+	
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Cosmetic code
+            new SampleTemplate.TemplateForm(this, "Business (CLR) Objects", "Chart FX supports reading data from a Business Object. In this case, we have created a class containing the&nbsp;data (Department) and used the ListProvider to populate Chart FX with multiple instances of the class.");
+            DoubleBuffered = true;
+        }
+
+        #region Data Population Functions
+        
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxSamples.Items.Clear();
+            comboBoxSamples.Items.Add("Business Objects");
+            
+            comboBoxSamples.SelectedIndex = 0;
+        }
+
+        private void comboBoxSamples_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            chart1.Reset();
+            string sampleName = comboBoxSamples.Items[comboBoxSamples.SelectedIndex].ToString();
+            string functionName = getFunctionName(sampleName);
+            Type thisClass = typeof(Form1);
+            thisClass.InvokeMember(functionName,BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Instance,null,this,null);
+        }
+
+        private string getFunctionName(string sampleName)
+        {
+            string functionName = "";
+            sampleName = sampleName.Replace("-", " ").Trim();
+            string[] words = sampleName.Split(' ');
+            foreach (string word in words)
+                functionName += char.ToUpper(word[0]) + word.Substring(1);
+
+            return functionName;
+        }
+
+        #region Chart Functions
+        private void BusinessObjects()
+	{
+            chart1.Gallery = Gallery.Bar;
+            chart1.Titles.Add(new TitleDockable("Expenses vs. Budget by Department"));
+            chart1.AxisY.LabelsFormat.Format = AxisFormat.Currency;
+            
+            Department[] departments = new Department[5];
+            departments[0] = new Department("Human Resources", 1500000, 1000000);
+            departments[1] = new Department("Sales", 1200000, 800000);
+            departments[2] = new Department("Marketing", 4500000, 5000000);
+            departments[3] = new Department("Development", 7320000, 5310000);
+            departments[4] = new Department("Other", 150000, 100000);
+            
+            ListProvider lstProvider = new ListProvider(departments);
+            chart1.DataSourceSettings.DataSource = lstProvider;
+        }
+
+                
+        #endregion
+
+    }
+
+    #region Data Classes
+    class Department
+        {
+            private string _name;
+            private double _budget;
+            private double _expenses;
+    
+            public Department(string name, double budget, double expenses)
+            {
+                _name = name;
+                _budget = budget;
+                _expenses = expenses;
+            }
+    
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+    
+            public double Budget
+            {
+                get { return _budget; }
+                set { _budget = value; }
+            }
+    
+            public double Expenses
+            {
+                get { return _expenses; }
+                set { _expenses = value; }
+            }
+        }
+
+    
+    #endregion
+}
